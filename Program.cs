dd5c6b1e09898c6f7d5f1a1f2719a4693630be7e
AhmedMaher215 <127059712+AhmedMaher215@users.noreply.github.com>
2024-07-21 18:43:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GraphQL.Types;
+using System.Reflection;
+using OnePiece1.formss;
+using DevExpress.XtraEditors;
+using OnePiece1.frm;
+using OnePiece1.forms;
+using DevExpress.LookAndFeel;
+using OnePiece1.Properties;
+using OnePiece1.dal;
+using OnePiece1.Class;
+
+namespace OnePiece1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        //Frm_Invoce(Class.master.InvoiceType.purchase) شاشة الفاتوره
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            UserLookAndFeel.Default.SkinName = Settings.Default.SkinName.ToString();
+            UserLookAndFeel.Default.SetSkinStyle(Settings.Default.SkinName.ToString(), Settings.Default.PaletteName.ToString());
+            new frm_Login().Show();
+            Application.Run();
+            
+        }
+     
+        
+    }
+}
